d35e227a2ce1d1b1c0fce4023b03f76b867ae630
SHIN_MINGYU <smg20004@naver.com>
2022-07-17 06:05:30 +0900

    data-structor stack

--- data-structor/10828.cc
+++ data-structor/10828.cc
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <vector>
+#include <string>
+#include <sstream>
 using namespace std;
 
 class Node{
@@ -7,6 +10,7 @@ class Node{
         Node(int value, Node* nextNode);
         void setNextNode(Node* nextNode);
         Node* getNextNode();
+        void setValue(int value);
         int getValue();
   private:
     int value;
@@ -30,50 +34,107 @@ Node* Node::getNextNode(){
 int Node::getValue(){
     return this->value;
 }
+void Node::setValue(int value){
+    this->value = value;
+}
+
 
 class Stack{
     public:
         void push(int value);
         int pop();
-        int size();
         bool empty();
         int top();
         Stack();
-        Node* headerNode;
+        int size = 0;
     private:    
-
+        Node* headerNode;
         Node* tailNode;
 };
 
 Stack::Stack(){
-    this->tailNode =new Node(123,0);
+    this->tailNode =new Node(0,0);
     this->headerNode = new Node(0,tailNode);
 }
 
 void Stack::push(int value){
     Node* newNode = new Node(value, this->tailNode);
     if(headerNode->getNextNode() == tailNode){
-        this ->headerNode -> setNextNode(newNode);
+        headerNode->setNextNode(newNode);
+        size += 1;
         return;
     }
-
     Node* nextNode = headerNode->getNextNode();
-    while(nextNode != tailNode){
-        nextNode = nextNode->getNextNode();
-        if(nextNode == tailNode){
-            nextNode->setNextNode(newNode);
-        }
-     }
-     return;
+    headerNode->setNextNode(newNode);
+    newNode->setNextNode(nextNode);
+    size += 1 ;
+    return;
+}
+
+int Stack::pop(){
+    if(headerNode->getNextNode() == tailNode){
+        return -1;
+    }
+    int popedValue = headerNode->getNextNode()->getValue();
+    Node* nextNode = headerNode->getNextNode()->getNextNode();
+    headerNode->setNextNode(nextNode);
+    this->size -= 1;
+    return popedValue;
+}
+
+bool Stack::empty(){
+    if(this->size == 0){
+        return 1;
+    }
+    return 0;
+}
+
+int Stack::top(){
+    if(this->headerNode->getNextNode() != tailNode){
+        return headerNode->getNextNode()->getValue();
+    }
+    return -1;
 }
 
 
+vector<string> split(string str, char Delimiter){
+    istringstream iss;
+    iss.str(str);
+    string buffer;
+    
+    vector<string> result;
 
+    while(iss >> buffer){
+        result.push_back(buffer);
+    }
+
+    return result;
+}
+
+bool startsWith(const std::string &str, const std::string &prefix) {
+    return str.size() >= prefix.size() && str.compare(0, prefix.size(), prefix) == 0;
+}
 
 int main(){
     Stack* stack = new Stack();
-    stack->push(1);
-    stack->push(2);
-    cout << stack->headerNode->getNextNode()->getNextNode()->getValue();
+    int N = 0;
+    cin >> N;
+    for(int i = 0; i <= N; i++){
+        string command;
+        getline(cin,command);
+        if(startsWith(command ,"push")){
+            vector<string> number = split(command,' ');
+            int pushNum = stoi(number[1]);
+            stack->push(pushNum); 
+        }else if (startsWith(command ,"pop")){
+            cout << stack->pop() << endl;
+        }else if (startsWith(command ,"size")){
+            cout << stack->size << endl;
+        }else if (startsWith(command ,"empty")){
+            cout << stack->empty() << endl;
+        }else if (startsWith(command ,"top")){
+            cout << stack->top() << endl;
+        }
+    }
     return 0;
 }
